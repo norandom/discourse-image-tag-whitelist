d8cf5f02ba954a01e05666961ac7fa506fd07b50
Marius Ciepluch <marius@ccpgames.com>
2017-07-20 13:25:12 +0000

    ressources renamed

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,5 @@
 # name: span-class
 # about: Whitelist a some custom CSS classes for span for typography experiments
 # version: 0.2
-# authors: scossar, norandom
+# authors: scossar, norandom, cpradio
+# url: https://github.com/norandom/discourse-span-tag-whitelist
